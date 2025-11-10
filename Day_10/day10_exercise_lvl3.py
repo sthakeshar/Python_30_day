dde4a99998c4690b8b1c12c12073a76b4033ea1c
sthakeshar <shresthakeshar291@gmail.com>
2025-11-10 15:39:47 +0900

    day 10 exercise modification

--- Day_10/day10_exercise_lvl3.py
+++ Day_10/day10_exercise_lvl3.py
@@ -2,8 +2,6 @@ import sys
 sys.path.append('\\Python_30_day')
 
 from DATA.countries import countries
-from DATA.countries_data import countries1
-
 
 for country in countries:
     if 'land' in country:
@@ -17,20 +15,3 @@ for fruit in fruits:
     rev_fruits.insert(0,fruit)
 
 print("the reversed fruit list is",rev_fruits)
-
-#What are the total number of languages in the data
-
-
-# Step 2: Create an empty set to store unique languages
-unique_languages = set()
-
-# Step 3: Loop through each country in the data
-for country in countries1:
-    for language in country['languages']:
-        unique_languages.add(language)
-
-# Step 4: Get the total number of unique languages
-total_languages = len(unique_languages)
-
-# Step 5: Print the result
-print("Total number of unique languages:", total_languages)
