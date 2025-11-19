0f6af00b25b760361f9a54a260e7acd49662b1c0
sthakeshar <shresthakeshar291@gmail.com>
2025-11-19 20:09:12 +0900

    day 17 exercises completed

---
+++ Day_17_exception_handling/day_17_exercise.py
@@ -0,0 +1,36 @@
+#names = ['Finland', 'Sweden', 'Norway','Denmark','Iceland', 'Estonia','Russia']. Unpack the first five countries and store them in a variable nordic_countries, store Estonia and Russia in es, and ru respectively.
+names = ['Finland', 'Sweden', 'Norway','Denmark','Iceland', 'Estonia','Russia']
+es,ru,*nordic_countries=names[::-1]
+print(nordic_countries)
+print(es)
+print(ru)
+
+#enumerae
+for index, item in enumerate([20, 30, 40]):
+    print(index, item)
+
+#zip
+fruits = ['banana', 'orange', 'mango', 'lemon', 'lime']                    
+vegetables = ['Tomato', 'Potato', 'Cabbage','Onion', 'Carrot']
+fruits_and_veges = []
+for f, v in zip(fruits, vegetables):
+    fruits_and_veges.append({'fruit':f, 'veg':v})
+
+print(fruits_and_veges)
+
+#----try----except----else----finally-----block
+try:
+    name = input('Enter your name:')
+    year_born = input('Year you born:')
+    age = 2019 - int(year_born)
+    print(f'You are {name}. And your age is {age}.')
+except TypeError:
+    print('Type error occur')
+except ValueError:
+    print('Value error occur')
+except ZeroDivisionError:
+    print('zero division error occur')
+else:
+    print('I usually run with the try block')
+finally:
+    print('I alway run.')
