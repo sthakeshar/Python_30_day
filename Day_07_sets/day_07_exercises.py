12cf7a5a38cb10d56998bd0c1c84c16352bc4335
sthakeshar <shresthakeshar291@gmail.com>
2025-11-28 20:14:02 +0900

    day 07 exercise lvl1 completed

---
+++ Day_07_sets/day_07_exercises.py
@@ -0,0 +1,20 @@
+# sets
+it_companies = {'Facebook', 'Google', 'Microsoft', 'Apple', 'IBM', 'Oracle', 'Amazon'}
+A = {19, 22, 24, 20, 25, 26}
+B = {19, 22, 20, 25, 26, 24, 28, 27}
+age = [22, 19, 24, 25, 26, 24, 25, 24]
+
+#Find the length of the set it_companies
+print("the length of it companies are: ",len(it_companies))
+
+#Add 'Twitter' to it_companies
+it_companies.add('twitter')
+print(it_companies)
+
+#Insert multiple IT companies at once to the set it_companies
+it_companies.update(['meta','netflix'])
+print(it_companies)
+
+#Remove one of the companies from the set it_companies
+it_companies.remove('IBM')
+print(it_companies)
